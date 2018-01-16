33ee82b74c828230b693eb37b2755a296fb5bf83
Charley Kirkpatrick <ckirkpat7@gmail.com>
2018-01-16 08:58:27 -0600

    basic logging working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res, next)=>res.send('hello'))
+app.use(function(req, res, next){
+    console.log(req.method, ' / ', res.statusCode);
+    next();
+})
+
+app.get('/',(req, res, next)=> {
+    res.send('hello');
+})
+app.get('/news',(req, res, next)=>res.send('THE NEWS'))
 
 app.listen(3000,()=>console.log('Listening'))
